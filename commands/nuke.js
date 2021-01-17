0a5db4002d68670a93a121c257fc59804d1695fd
Drago-Dev24 <77574238+Drago-Dev24@users.noreply.github.com>
2021-01-17 18:23:22 +0530

    nuke.js
    A Simple Nuke Command, Works Amazing

---
+++ commands/nuke.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js') 
+module.exports = {
+  name: "nuke",
+  category: "moderation",
+  run : async(client, message, args) => {
+//Make this fit your command handler style 😀
+
+if (!message.member.hasPermission("MANAGE_CHANNELS")) {  
+  return message.reply(**You Need (MANAGE_CHANNELS) Permission To Run This Command}   
+  let channel = client.channels.cache.get(message.channel.id)
+  
+let Position = channel.position;   
+  channel.clone().then((channel2) => {   
+    channel2.setPosition(Position)  
+    channel.delete()   
+    channel2.send("Channel Has been nuked !",{   
+      files: ['https://media.tenor.com/images/0754697c9c4dd44ca8504dbf1b36b927/tenor.gif']   
+    }) 
+  })   
+}
+}
