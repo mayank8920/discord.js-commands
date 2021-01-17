5a277151e0821cfcdc839b1c6719a28c6e69e280
Drago-Dev24 <77574238+Drago-Dev24@users.noreply.github.com>
2021-01-17 18:28:19 +0530

    listroles.js
    A Command To List All The Roles Available In The Guild

---
+++ commands/listroles.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js') 
+module.exports = {  
+  name: 'listroles',  
+  category: "utility",
+  run: async (client, message, args) => {  
+\\ make this fit your command handler style 😀
+
+    let rolemap = message.guild.roles.cache      
+    .sort((a, b) => b.position - a.position)   
+    .map(r => r)         
+    .join("\n");       
+    if (rolemap.length > 1024) rolemap = "To many roles to display";     
+    if (!rolemap) rolemap = "No roles to display"; 
+    const embed = new Discord.MessageEmbed()   
+    .addField("Role List" , rolemap)   
+    .setColor("00ffcc")
+    message.channel.send(embed);
+  }
+} 
